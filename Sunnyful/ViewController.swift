1685916c1dea754be92446f432136c7e7518121a
ykson <mrson071@gmail.com>
2020-01-05 17:24:13 +0900

    Initial Commit

---
+++ Sunnyful/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sunnyful
+//
+//  Created by leo on 2020/01/05.
+//  Copyright © 2020 leo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
